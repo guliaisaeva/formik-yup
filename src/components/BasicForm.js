632519200e6bf1abd56b8087dab2266020f8b70f
Gulia <gulya.isaeva.pls@gmail.com>
2024-09-06 17:50:02 +0300

    refactor: properties and names

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,30 @@
 import { useFormik } from "formik";
+import { basicSchema } from "../schemas";
 
+const onSubmit = ()=>{
+  console.log("submitted")
+}
 const BasicForm = () => {
-  const {value,handleBlur,handleChange} =useFormik({
+  const {values,errors,touched,handleBlur,handleChange,handleSubmit} =useFormik({
     initialValues:{
       email:"",
       age:"",
       password:"",
       confirmPassword:""
-    }
+    },
+    validationSchema:basicSchema,
+    onSubmit
   })
-  console.log(formik)
   return (
-    <form autoComplete="off">
+    <form onSubmit={handleSubmit} autoComplete="off">
       <label htmlFor="email">Email</label>
-      <input onChange={handleChange} value={values.email} id="email" type="email" placeholder="Enter your email" onBlur={formik.handleBlur}/>
+      <input onChange={handleChange} value={values.email} id="email" type="email" placeholder="Enter your email" onBlur={handleBlur} className={errors.email && touched.email?"input-error":""}/>
       <label htmlFor="age">Age</label>
-      <input onChange={handleChange} value={values.age} id="age" type="number" placeholder="Enter your age" onBlur={formik.handleBlur}/>
+      <input onChange={handleChange} value={values.age} id="age" type="number" placeholder="Enter your age" onBlur={handleBlur}/>
       <label htmlFor="password">Password</label>
-      <input onChange={handleChange} value={values.password} id="password" type="password" placeholder="Enter your password" onBlur={formik.handleBlur} />
+      <input onChange={handleChange} value={values.password} id="password" type="password" placeholder="Enter your password" onBlur={handleBlur} />
       <label htmlFor="confirmPassword">Confirm Password</label>
-      <input onChange={handleChange} value={values.confirmPassword} id="confirmPassword" type="password" placeholder="Confirm password" onBlur={formik.handleBlur} />
+      <input onChange={handleChange} value={values.confirmPassword} id="confirmPassword" type="password" placeholder="Confirm password" onBlur={handleBlur} />
   <button type="submit">Submit</button>
    </form>
   );
