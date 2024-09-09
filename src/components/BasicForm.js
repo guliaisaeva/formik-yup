3a826c739a1ff056a0d1978141a7912ebcebdfa8
Gulia <gulya.isaeva.pls@gmail.com>
2024-09-09 11:51:14 +0300

    feat:add advanced version

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,11 +1,16 @@
 import { useFormik } from "formik";
 import { basicSchema } from "../schemas";
 
-const onSubmit = ()=>{
+const onSubmit = async(values,actions)=>{
   console.log("submitted")
+  console.log(values)
+  console.log(actions)
+
+  await new Promise((resolve)=>setTimeout(resolve,1000))
+  actions.resetForm()
 }
 const BasicForm = () => {
-  const {values,errors,touched,handleBlur,handleChange,handleSubmit} =useFormik({
+  const {values,errors,touched,handleBlur,handleChange,handleSubmit,isSubmitting} =useFormik({
     initialValues:{
       email:"",
       age:"",
@@ -18,14 +23,21 @@ const BasicForm = () => {
   return (
     <form onSubmit={handleSubmit} autoComplete="off">
       <label htmlFor="email">Email</label>
-      <input onChange={handleChange} value={values.email} id="email" type="email" placeholder="Enter your email" onBlur={handleBlur} className={errors.email && touched.email?"input-error":""}/>
+      <input onChange={handleChange} value={values.email} id="email" type="email" placeholder="Enter your email" onBlur={handleBlur} className={errors.email && touched.email? "input-error":""}/>
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
       <label htmlFor="age">Age</label>
-      <input onChange={handleChange} value={values.age} id="age" type="number" placeholder="Enter your age" onBlur={handleBlur}/>
+      <input onChange={handleChange} value={values.age} id="age" type="number" placeholder="Enter your age" onBlur={handleBlur}  className={errors.age && touched.age? "input-error":""}/>
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
+
       <label htmlFor="password">Password</label>
-      <input onChange={handleChange} value={values.password} id="password" type="password" placeholder="Enter your password" onBlur={handleBlur} />
+      <input onChange={handleChange} value={values.password} id="password" type="password" placeholder="Enter your password" onBlur={handleBlur}  className={errors.password && touched.password? "input-error":""} />
+      {errors.password && touched.password && <p className="error">{errors.password}</p>}
+
       <label htmlFor="confirmPassword">Confirm Password</label>
-      <input onChange={handleChange} value={values.confirmPassword} id="confirmPassword" type="password" placeholder="Confirm password" onBlur={handleBlur} />
-  <button type="submit">Submit</button>
+      <input onChange={handleChange} value={values.confirmPassword} id="confirmPassword" type="password" placeholder="Confirm password" onBlur={handleBlur}  className={errors.confirmPassword && touched.confirmPassword? "input-error":""} />
+      {errors.confirmPassword && touched.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+
+  <button disabled={isSubmitting} type="submit">Submit</button>
    </form>
   );
 };
